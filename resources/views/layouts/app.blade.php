2127b61cf7e460a2e1d846516a38b413ea65b816
raoulkr <raoulkr@live.nl>
2022-01-09 16:50:13 +0100

    added project starters page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" />
+        <title>{{config('app.name', 'NetflixAPP')}}</title>
+    </head>
+
+    <body>
+        <nav class = "NavBar">
+
+            <ul>
+                <li class="{{ Request::is('/') ? 'active' : '' }}"><a href={{ url('/') }}>Home</a></i>
+            </ul>
+        </nav>
+        <div class="yield">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>welcome</h1>
+    <p>this is the body</p>
+@endsection
